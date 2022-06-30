4d174d38250434d833dc58d71759e88a0a627592
Abaid Ur Rehman <abar@sitecore.net>
2022-06-30 15:50:33 +0200

    This should trigger another build and update page

--- src/rendering/src/pages/_app.tsx
+++ src/rendering/src/pages/_app.tsx
@@ -23,7 +23,7 @@ function App({ Component, pageProps }: AppProps): JSX.Element {
   return (
     // Use the next-localization (w/ rosetta) library to provide our translation dictionary to the app.
     // Note Next.js does not (currently) provide anything for translation, only i18n routing.
-    // If your app is not multilingual, next-localization and references to it can be removed..
+    // If your app is not multilingual, next-localization and references to it can be removed...
     <I18nProvider lngDict={dictionary} locale={pageProps.locale}>
       <Component {...rest} />
     </I18nProvider>
